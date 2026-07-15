cd9084cb3c8baaa205a077a2551c7567f5e4c94e
Time And Time Studio <nok.craft.studio@gmail.com>
2026-07-15 22:05:33 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,9 +4,13 @@ sudo dnf install thunar
 
 # sudo dnf install dnf-plugins-core
 
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
 sudo dnf install brave-origin
+
+cd Downloads
+wget https://github.com/TimeAndTimeStudio/Game-Web-Site/blob/main/019b913a-a484-7237-b907-2444e37e22e2.png
+wget https://github.com/TimeAndTimeStudio/Game-Web-Site/blob/main/horizontal%20pic.jpg
 
 sudo systemctl enable gdm --now
 sudo systemctl set-default graphical.target
