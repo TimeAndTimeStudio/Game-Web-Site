8da7cb86e372c22f0e6af57993f0b2fed5ecdb13
Time And Time Studio <nok.craft.studio@gmail.com>
2026-07-15 23:25:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sudo dnf install gdm gnome-terminal 
 sudo dnf remove gnome-tour
-sudo dnf install nautilus gnome-software
+sudo dnf install nautilus
 
 sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
 
